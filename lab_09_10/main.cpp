82d2e6d89146dc9c207c1e1b2520cf8f255a4547
ipsupp <121975704+ipsupp@users.noreply.github.com>
2023-05-09 00:53:40 +0300

    Delete lab_09_10 directory

--- lab_09_10/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Repo.h"
-#include "teste.h"
-
-using namespace std;
-int main() {
-    test_all();
-}
--- lab_09_10/teste.h
+++
@@ -1,4 +0,0 @@
-
-void tenisi_setteri_getteri();
-void repo_add_delete_update();
-void test_all();
